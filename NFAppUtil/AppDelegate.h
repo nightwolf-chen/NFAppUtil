e189beb043b791c05d5d83450155dc493c0ad0d2
Jidong <466202783@qq.com>
2015-05-05 10:54:00 +0800

    Initial Commit

---
+++ NFAppUtil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NFAppUtil
+//
+//  Created by ChenJidong on 15/5/5.
+//  Copyright (c) 2015年 nirvawolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NFAppUtil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NFAppUtil
+//
+//  Created by ChenJidong on 15/5/5.
+//  Copyright (c) 2015年 nirvawolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
